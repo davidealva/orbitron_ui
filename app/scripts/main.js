3d4403a0b73fcdc6d616d843b85892b5fb580ea2
balihoo-dalvarado <dalvarado@balihoo.com>
2016-10-15 23:30:10 -0600

    Initial Commit
    Initial Commit

---
+++ app/scripts/main.js
@@ -0,0 +1,8 @@
+$( document ).ready(function(){
+		'use strict';
+    $('.element').typed({
+        strings: ['All the benefits of <br>a Private Cloud ^1000<br>without the headaches^1500', 'All the benefits of <br>a Private Cloud ^1000<br>without the cost^1500'],
+        typeSpeed: 0,
+        cursorChar: '_'
+    });
+});
